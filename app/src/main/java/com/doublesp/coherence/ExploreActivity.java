c44a9ec33d0a87d4cc397523c98184f5907c1f3c
Santhosh Sachindran <sans2020@gmail.com>
2016-11-11 20:33:07 -0800

    Integrated Crashlytics reporting to the app.

--- app/src/main/java/com/doublesp/coherence/ExploreActivity.java
+++ app/src/main/java/com/doublesp/coherence/ExploreActivity.java
@@ -1,13 +1,18 @@
 package com.doublesp.coherence;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class ExploreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_explore);
     }
 }
