fe24f29f622bbd45e2ce3520fbdecba3899d5bc8
pinyaoting <pinyaoting@gmail.com>
2016-11-18 15:49:50 -0800

    solbe conflict

--- app/src/main/java/com/doublesp/coherence/utils/CoherenceTabUtils.java
+++ app/src/main/java/com/doublesp/coherence/utils/CoherenceTabUtils.java
@@ -41,11 +41,6 @@ public class CoherenceTabUtils {
 
         layout.getTabAt(0).setIcon(R.drawable.ic_create);
         layout.getTabAt(1).setIcon(R.drawable.ic_library_books);
-<<<<<<< HEAD
-=======
-        layout.getTabAt(2).setIcon(R.drawable.ic_supervisor_account);
-        layout.getTabAt(3).setIcon(R.drawable.ic_public);
->>>>>>> master
     }
 
 }
