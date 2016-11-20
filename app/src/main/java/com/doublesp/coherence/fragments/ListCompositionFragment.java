d7080038a6e770261983345d78b70f8c6aceb25a
pinyaoting <pinyaoting@gmail.com>
2016-11-20 14:10:32 -0800

    add animated background

--- app/src/main/java/com/doublesp/coherence/fragments/ListCompositionFragment.java
+++ app/src/main/java/com/doublesp/coherence/fragments/ListCompositionFragment.java
@@ -8,19 +8,28 @@ import com.doublesp.coherence.interfaces.presentation.ListFragmentActionHandlerI
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 public class ListCompositionFragment extends Fragment {
 
+    static final int LIST_COMPOSITION_BACKGROUND_IMAGE_ROTATION_INTERVAL = 5000;
     FragmentListCompositionBinding binding;
+    int[] mBackgroundImageIds;
+    int mBackgroundImageIndex;
     @Inject
     @Named("Composition")
     RecyclerView.Adapter<RecyclerView.ViewHolder> mAdapter;
@@ -42,6 +51,7 @@ public class ListCompositionFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            setupBackgroundImageId();
         }
     }
 
@@ -53,6 +63,7 @@ public class ListCompositionFragment extends Fragment {
         binding.rvIdeas.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.rvIdeas.setAdapter(mAdapter);
         binding.setHandler(mActionHandler);
+        rotateImage();
         return binding.getRoot();
     }
 
@@ -68,6 +79,45 @@ public class ListCompositionFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    void setupBackgroundImageId() {
+        mBackgroundImageIds = new int[5];
+        mBackgroundImageIds[0] = R.drawable.background_0;
+        mBackgroundImageIds[1] = R.drawable.background_1;
+        mBackgroundImageIds[2] = R.drawable.background_2;
+        mBackgroundImageIds[3] = R.drawable.background_3;
+        mBackgroundImageIds[4] = R.drawable.background_4;
+        mBackgroundImageIndex = 0;
+    }
+
+    void rotateImage() {
+        binding.ivIdeaCompositionBackground.setImageResource(mBackgroundImageIds[mBackgroundImageIndex]);
+        binding.ivIdeaCompositionBackground.startAnimation(fadeInOutAnimation());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mBackgroundImageIndex = (mBackgroundImageIndex + 1) % mBackgroundImageIds.length;
+                rotateImage();
+            }
+        }, LIST_COMPOSITION_BACKGROUND_IMAGE_ROTATION_INTERVAL);
+    }
+
+    Animation fadeInOutAnimation() {
+        int duration = LIST_COMPOSITION_BACKGROUND_IMAGE_ROTATION_INTERVAL / 2;
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator());
+        fadeIn.setDuration(duration);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(duration);
+        fadeOut.setDuration(duration);
+
+        AnimationSet animation = new AnimationSet(true);
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+        return animation;
     }
 
 }
---
+++ app/src/main/java/com/doublesp/coherence/utils/MVAccelerateDecelerateInterpolator.java
@@ -0,0 +1,21 @@
+package com.doublesp.coherence.utils;
+
+import android.view.animation.Interpolator;
+
+/**
+ * Created by pinyaoting on 11/20/16.
+ */
+
+public class MVAccelerateDecelerateInterpolator implements Interpolator {
+    @Override
+    public float getInterpolation(float v) {
+        float x;
+        if (v < 0.5f) {
+            x = v * 2.0f;
+            return 0.5f * x * x * x * x * x;
+        }
+        x = (v - 0.5f) * 2 - 1;
+        return 0.5f * x * x * x * x * x + 1;
+
+    }
+}
