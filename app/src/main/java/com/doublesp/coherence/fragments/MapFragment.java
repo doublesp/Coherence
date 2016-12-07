ead91a59d53b6071c36b801d36b283c4e3fcd986
Shawn Duan <duan1216@gmail.com>
2016-12-06 23:06:48 -0800

    Fetching current location intervally.

--- app/src/main/java/com/doublesp/coherence/fragments/MapFragment.java
+++ app/src/main/java/com/doublesp/coherence/fragments/MapFragment.java
@@ -2,6 +2,7 @@ package com.doublesp.coherence.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,7 +16,12 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.doublesp.coherence.R;
+import com.doublesp.coherence.googleplace.gplace.Result;
 import com.doublesp.coherence.utils.LocationCluster;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -25,8 +31,14 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterManager;
 
+import java.util.ArrayList;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+
+public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
 
     private static String TAG = MapFragment.class.getSimpleName();
 
@@ -36,6 +48,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     boolean mIsMapInit;
     // Declare a variable for the cluster manager.
     private ClusterManager<LocationCluster> mClusterManager;
+
+
+    private GoogleApiClient mGoogleApiClient;
+    private ArrayList<Result> mStoreList;
+    private Subscription searchNearbyStoresSubscription;
+    private LatLng lastKnownLocation;
+    private LocationRequest mLocationRequest;
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
 
     public MapFragment() {
@@ -110,10 +132,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
             return;
         }
-        mMap.setMyLocationEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
 
-        setUpClusterer();
+//        mMap.setMyLocationEnabled(true);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+//
+//        setUpClusterer();
+
+        LatLng lastKnownLocation = getLastKnownLocation();
+
+
+        String latAndLng;
+
+        latAndLng = (lastKnownLocation != null) ? Double.toString(lastKnownLocation.latitude) + "," +
+                Double.toString(lastKnownLocation.longitude) : null;
+
+        subscribeNearbyStores(latAndLng);
 
     }
 
@@ -200,6 +233,64 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             LocationCluster offsetItem = new LocationCluster(lat, lng);
             mClusterManager.addItem(offsetItem);
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        lastKnownLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+    }
+
+    private LatLng getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Get last known recent location.
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        // Note that this can be NULL if last location isn't already known.
+        if (mCurrentLocation != null) {
+            // Print current location if not null
+            Log.d("DEBUG", "current location: " + mCurrentLocation.toString());
+            LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        }
+        // Begin polling for new location updates.
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (i == CAUSE_SERVICE_DISCONNECTED) {
+            Toast.makeText(getActivity(), "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+        } else if (i == CAUSE_NETWORK_LOST) {
+            Toast.makeText(getActivity(), "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        // Request location updates
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void subscribeNearbyStores(String latAndLng) {
+        searchNearbyStoresSubscription = GooglePlaceClient.newInstance()
+                .searchStores(latAndLng)
+                .subscribeOn(Schedulers.io()) // optional if you do not wish to override the default behavior
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SearchNearbyStoresSubscriber(true));
     }
 
 }
